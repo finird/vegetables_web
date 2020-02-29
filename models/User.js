0a59eac045057ca37034dede88e6fac27def4bd4
dltruongc <dltruong.c@gmail.com>
2020-02-29 15:42:50 +0700

    user fixed

--- models/User.js
+++ models/User.js
@@ -1,38 +1,39 @@
 const { Schema } = require('mongoose');
 const bcrypt = require('bcrypt');
+
 const userSchema = new Schema({
   last_name: {
     type: String,
     default: '',
-    min: [2, "Least 2 characters"],
-    max: [20, "Max 20 characters"],
-    trim: true,
+    min: [2, 'Least 2 characters'],
+    max: [20, 'Max 20 characters'],
+    trim: true
   },
   first_name: {
     type: String,
     default: '',
-    min: [2, "Least 2 characters"],
-    max: [20, "Max 20 characters"],
-    trim: true,
+    min: [2, 'Least 2 characters'],
+    max: [20, 'Max 20 characters'],
+    trim: true
   },
   username: {
     type: String,
-    min: [2, "Least 2 characters"],
-    max: [40, "Max 40 characters"],
-    require: [true, "username is required!"],
-    unique: [true, "username existed"],
-    trim: true,
+    min: [2, 'Least 2 characters'],
+    max: [40, 'Max 40 characters'],
+    require: [true, 'username is required!'],
+    unique: [true, 'username existed'],
+    trim: true
   },
   email: {
     type: String,
     min: 10,
     max: 40,
-    require: [true, "username is required!"],
-    unique: [true, "email existed"],
+    require: [true, 'username is required!'],
+    unique: [true, 'email existed'],
     validate: {
       validator: function(value) {
-        var res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(value).toLocaleLowerCase());
+        const res = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return res.test(String(value).toLocaleLowerCase());
       },
       message: props => `${props.value} is not a valid email`
     }
@@ -41,7 +42,7 @@ const userSchema = new Schema({
     type: String,
     min: 8,
     max: 50,
-    require: [true, "password is required!"],
+    require: [true, 'password is required!']
   },
   roles: {
     type: String,
@@ -55,7 +56,7 @@ const userSchema = new Schema({
   },
   job: {
     type: String,
-    trim: true,
+    trim: true
   },
   createdAt: {
     type: Date,
@@ -65,34 +66,34 @@ const userSchema = new Schema({
     type: String,
     default: '',
     max: 100,
-    trim: true,
+    trim: true
   },
-  photo:{
-    type: String,
+  photo: {
+    type: String
   },
   about: {
     type: String,
-    trim: true,
+    trim: true
   }
 });
 
-userSchema.pre(save, function(next) {
+userSchema.pre('save', function(next) {
   const user = this;
   user.salt = bcrypt.genSaltSync(12);
   bcrypt.hash(user.password, this.salt, function(err, hash) {
-    if(err) return next(err);
+    if (err) return next(err);
     user.password = hash;
     next();
-  })
+  });
 });
 
 userSchema.methods = {
   authenticate: function(password) {
     bcrypt.compare(password, this.password, function(err, isMatch) {
-      if(err){
+      if (err) {
         throw new Error(err);
       }
       return isMatch;
-    })
+    });
   }
 };
