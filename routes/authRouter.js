cc3fca384100d83aaac0defef831195603cddb36
chithien0909 <chithien0909@gmail.com>
2020-03-06 12:42:01 +0700

    Fix auth register api

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,15 +4,17 @@ const {
   getUserById,
   registerUser,
   loginUser,
-  currentUser
+  currentUser,
+  editUser
 } = require('../controllers/authControllers');
 
 const authMiddleware = require('../middlewares/auth.middleware');
 
+router.route('/register').post(registerUser);
 router.route('/current-user').get(authMiddleware, currentUser);
 router.route('/getAll').get(getAllUsers);
-router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/edit/:id').post(authMiddleware, editUser);
 router.route('/:id').get(getUserById);
 
 module.exports = router;
